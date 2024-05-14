a94f6a1266b48cbb8c0872ba2bc24b6168d768a9
amh23 <ayemyahan23@gmail.com>
2024-05-14 16:25:26 +0630

    LogInPage finished

--- front-end/src/pages/LogInPage.js
+++ front-end/src/pages/LogInPage.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 export const LogInPage = () => {
+    const [errorMessage, setErrorMessage] = useState('');
+
     const [emailValue, setEmailValue ] = useState('');
     const [passwordValue, setPasswordValue ] = useState('');
 
@@ -14,6 +16,7 @@ export const LogInPage = () => {
     return(
         <div className="content-container">
             <h1>Log In</h1>
+            {errorMessage && <div className="fail">{errorMessage}</div>}
             <input
                 value={emailValue}
                 onChange={e => setEmailValue(e.target.value)}
@@ -24,9 +27,12 @@ export const LogInPage = () => {
                 onChange={e => setPasswordValue(e.target.value)}
                 placeholder="password" />
             <button
+                disabled={!emailValue || !passwordValue}
                 onClick={onLogInClicked}>Log In</button>
-            <button>Forgot your password</button>
-            <button>Don't have an account? Sign up</button>
+            <button
+                onClick={() => history.push('/forgot-password')}>Forgot your password</button>
+            <button
+                onClick={() => history.push('/signup')}>Don't have an account? Sign up</button>
         </div>
     );
 }
