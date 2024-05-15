f7ef19517cfee1672a89be3c04dc8c1e03614f05
amh23 <ayemyahan23@gmail.com>
2024-05-15 07:08:05 +0630

    JSON web token is generated

--- back-end/src/routes/SignUpRoute.js
+++ back-end/src/routes/SignUpRoute.js
@@ -32,5 +32,23 @@ export const signUpRoute = {
 
         const { insertedId } = result;
 
+        jwt.sign({
+            id: insertedId,
+            email,
+            info: startingInfo,
+            isVerified: false,
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: '2d',
+        },
+        (err, token) => {
+            if(err) {
+                return res.status(500).send(err);
+            }
+            res.status(200).json({ token });
+        }
+        );
+
     }
 }
--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,5 +1,7 @@
+import { signUpRoute } from './SignUpRoute';
 import { testRoute } from './testRoute';
 
 export const routes = [
+    signUpRoute,
     testRoute,
 ];
