f339c29205e085af5c00602ab0813fd8cc57494a
amh23 <ayemyahan23@gmail.com>
2024-05-15 07:25:28 +0630

    Added LogInRoute to the server

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,7 +1,9 @@
+import { logInRoute } from './logInRoute';
 import { signUpRoute } from './SignUpRoute';
 import { testRoute } from './testRoute';
 
 export const routes = [
+    logInRoute,
     signUpRoute,
     testRoute,
 ];
