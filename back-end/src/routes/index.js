130a59baf2f65a9a3b95623c5087b490c968fa39
amh23 <ayemyahan23@gmail.com>
2024-06-17 12:33:52 +0630

    googleOauthCallbackRoute is added

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,5 +1,6 @@
 import { forgotPasswordRoute } from './forgotPasswordRoute';
 import { getGoogleOauthUrlRoute } from './getGoogleOauthUrlRoute';
+import { googleOauthCallbackRoute } from './googleOauthCallbackRoute';
 import { logInRoute } from './logInRoute';
 import { resetPasswordRoute } from './resetPassword';
 import { signUpRoute } from './SignUpRoute';
@@ -11,6 +12,7 @@ import { verifyEmailRoute } from './verifyEmailRoute';
 export const routes = [
     forgotPasswordRoute,
     getGoogleOauthUrlRoute,
+    googleOauthCallbackRoute,
     logInRoute,
     resetPasswordRoute,
     signUpRoute,
